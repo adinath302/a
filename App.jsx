de5eb41a33a4d741d722b0db5515085029137ed3
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-06-27 22:36:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+import ProfileCards from './components/ProfileCards'
+import profileimges from "./assets/profile.jpg"
+import profile2 from "./assets/profile2.jpg"
+import Button from "./components/Button.jsx"
+import Card from './components/Cards.jsx'
+import Card1 from './components/Card1.jsx'
+import Inputsection from './components/Inputsection.jsx'
+import Impression from './components/impression.jsx'
+import Cardsection1 from './components/Cardsection1.jsx'
+import Teamsection from './components/Teamsection.jsx'
+import Testimonials from './components/Testimonials.jsx'
+import Login from './components/login/Login.jsx'
+import Todo from './components/Todo/Todo.jsx'
+import ProductCard from './components/Product-Card/ProductCard.jsx'
+import SignUp from './components/SignUp/SignUp.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
+import Books from './components/books/books.jsx'
+import NewTodo from './components/NewTodo/NewTodo.jsx'
+
+function App() {
+
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <Inputsection /> */}
+      {/* <Impression /> */}
+      {/* <Cardsection1 /> */}
+      {/* <Teamsection /> */}
+      {/* <Testimonials /> */}
+      {/* <Todo /> */}
+      {/* <ProductCard /> */}
+      {/* <SignUp /> */}
+      {/* <Books /> */}
+      <NewTodo />
+    </>
+  )
+}
+
+export default App;
